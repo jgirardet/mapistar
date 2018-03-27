b67c0bba6afc793eaeb9587a1cea3e4add0da3f8
jgirardet <ijkl@tuta.io>
2018-03-27 23:17:44 +0200

    env

--- mapistar/models.py
+++ mapistar/models.py
@@ -9,11 +9,25 @@ from .users import User
 from mapistar.actes.models import Acte, Observation
 # from config.settings import PONY
 # db.bind(**PONY['DATABASE'])
-db.connect(
-    provider="sqlite",
-    filename="db.sqlite3",
-    create_tables=True,
-    create_db=True,
-    # allow_auto_upgrade=True,
-)
-# db.generate_mapping(create_tables=True)
+import os
+from urllib.parse import urlparse
+
+try:
+    os.environ['TEST_RUNNING']
+except KeyError:
+    url = urlparse(os.environ['MAPISTAR_PONY_DB'])
+    db.connect(
+        provider=url.scheme,
+        host=url.hostname,
+        database=url.path.strip('/'),
+        user=url.username,
+        password=url.password,
+        port=url.port,
+        create_tables=True,
+    )
+else:
+    db.connect(
+        provider="sqlite",
+        filename=":memory:",
+        create_tables=True,
+    )
