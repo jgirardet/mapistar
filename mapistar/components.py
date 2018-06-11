70198ce90651ed513a9f678a8104245e316a11f8
Jimmy Girardet <test@ma.fr>
2018-06-11 12:28:32 +0200

    remove userC

--- mapistar/components.py
+++
@@ -1,9 +0,0 @@
-from typing import TypeVar
-
-from apistar import Component
-from apistar_jwt import JWTUser
-
-from mapistar.base_db import db
-from mapistar.utils import get_or_404
-
-UserC = TypeVar("UserC")
